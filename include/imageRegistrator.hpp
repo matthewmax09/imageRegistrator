602848ac5df656137ae9a473ce89444b2155576e
Matthew Ng <matthew_ng@sutd.edu.sg>
2025-07-28 18:23:40 +0800

    remove r2c logpolar declaration

--- include/imageRegistrator.hpp
+++ include/imageRegistrator.hpp
@@ -39,8 +39,5 @@ public:
 
     template <typename T>
     void logPolarTransform(std::vector<std::complex<double>> &img, std::vector<T> &output);
-    template <typename T>
-    void logPolarTransform(std::vector<double> &img, std::vector<T> &output);
-
     std::pair<double, double> getAngScale(std::vector<std::complex<double>> &img1,std::vector<std::complex<double>> &img2);
 };
